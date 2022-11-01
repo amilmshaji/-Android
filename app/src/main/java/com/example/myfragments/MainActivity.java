3ee7244f958f461cc5f16b3b20306972eec73973
amilmshaji <msamilmanadiyil2560@gmail.com>
2022-11-01 13:06:35 +0530

    new

--- app/src/main/java/com/example/myfragments/MainActivity.java
+++ app/src/main/java/com/example/myfragments/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myfragments;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
@@ -31,6 +34,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+    }
+
+    private void loadFragment(Fragment f) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.frag1,f);
+        ft.commit();
 
     }
 }
